0a076a03aed0106c3852b34b227a0144ab2a579b
tebogo60 <tjjmalebana@gmail.com>
2025-09-30 22:27:21 +0200

    feat(routes): add home and login page routes

--- routes/routes.php
+++ routes/routes.php
@@ -1,9 +1,16 @@
 <?php
 
 use FastRoute\RouteCollector;
+use App\Controller\HomeController;
+
+$controller;
 
 return function (RouteCollector $r) {
     $r->addRoute('GET', '/', function () {
-        echo "Home Page";
+        require __DIR__ . "/../app/View/pages/home.php";
+    });
+
+    $r->addRoute('GET', '/login', function () {
+        require __DIR__ . "/../app/View/auth/login.php";
     });
 };
